2a3834110cbc7f77bf7a72d257ee152705a30943
Victor Ngeno <ngenovictor321@gmail.com>
2017-06-14 11:08:59 +0300

    change functionality to append trash icons and delete tr html tag on click

--- js/script.js
+++ js/script.js
@@ -1,38 +1,21 @@
-// function hideItems(htmlClass){
-//   $(htmlClass).remove();
-//
-// }
-// function removeItem(htmlClass){
-// $("htmlClass").click(function(){
-//
-// })
-//   
-// }
-//
-// $(document).ready(function(){
-//   var numItems = 1;
-//   $("form#stuff").submit(function(event){
-//     event.preventDefault();
-//
-//     var value = $("input#item1").val();
-//     $(".result").append("<tr class='d"+numItems+"'> <td>"+"<i class='fa fa-2x fa-trash trashitem"+numItems+"' aria-hidden='true'></i>"+" "+value+"<td> </tr>");
-//
-//     numItems+=1;
-//
-//     $(".result").show();
-//
-//     for(var j=1; j<=numItems; j++){
-//       $("i.fa.fa-trash.trashitem"+j).unbind(click())
-//       $("i.fa.fa-trash.trashitem"+j).click(hideItems(".tr.d"+j));
-//     }
-//   })
-//
-// })
-//
-// // Make a to do list webpage where people can add tasks to
-// //  create a list of things to do. Add an option for users
-// //  to indicate a task is done. Use jQuery to remove the
-// //  task from the "to do" list. Make sure to identify
-// //  specific behaviors your program should demonstrate,
-// //  and create specifications with example input and output.
-// //  List these in the project's README
+$(document).ready(function(){
+  $("form#stuff").submit(function(event){
+    event.preventDefault();
+
+    var value = $("input#item1").val();
+    $(".result").append("<tr><td><i class='fa fa-2x fa-trash' aria-hidden='true'></i>"+" "+value+"<td></tr>");
+
+    $(".result").show();
+
+    $("input#item1").val("");
+
+    $(".fa-trash").click(function(){
+      $(this).parent().parent().fadeOut(500, function(){
+        $(this).remove();
+      });
+      event.stopPropagation();
+    });
+
+  });
+
+});
